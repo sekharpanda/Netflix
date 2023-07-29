d11ae39d2510a71161f933ba96ab7f020abde4bf
Sekhar <sekharp720@gmil.com>
2023-07-29 08:00:07 +0530

    add gitignore file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
 import Banner from './Banner';
+import Loginscreen from './LoginScreen';
 
 
 function App() {
+
+  const user=null;
   return (
     <div className="App">
+
+
+    <Router>
+      {!user ?(<Loginscreen/>):(
+           <Routes>
+           <Route path="/" element={<HomeScreen/>}></Route>
+           <Route path="/test" element={<h1>I am Test</h1>}></Route>
+
+           </Routes>
+         
+        
+         )}
    
-    <HomeScreen/>
+    </Router>
   
- 
+
     </div>
   );
 }
---
+++ src/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LoginScreen.css";
+
+function Loginscreen() {
+
+    return <div className="Loginscreen">
+
+        
+    </div>;
+}
+
+export default Loginscreen;
